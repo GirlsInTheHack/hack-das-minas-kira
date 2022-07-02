8560996eaa57986782ab79ff77dbfa1de0550dd3
HannahFreitas <hannahcassia@gmail.com>
2022-07-01 23:00:59 -0300

    Subindo o banco de dados

--- src/app.js
+++ src/app.js
@@ -1,13 +1,16 @@
 require("dotenv-safe").config();
 const express = require("express");
 const cors = require("cors");
+const db = require("./database/mongoConfig");
 
 
 const app = express();
 
+db.connect();
 
-app.use(express.json());
 app.use(cors());
+app.use(express.json());
+
 
 
 
