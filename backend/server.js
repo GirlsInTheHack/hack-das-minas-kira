e78f4384405ee33571317623e89c96f4baec030b
HannahFreitas <hannahcassia@gmail.com>
2022-07-03 12:31:54 -0300

    adicionando o procfile

--- backend/server.js
+++ backend/server.js
@@ -4,11 +4,11 @@ const app = require('./src/app');
 const PORT = process.env.PORT || 5000;
 
 // Serve any static files
-app.use(express.static(path.join(__dirname, 'src/build')));
+app.use(express.static(path.join(__dirname, 'build')));
   
 // Handle React routing, return all requests to React app
 app.get('*', function(req, res) {
-  res.sendFile(path.join(__dirname, 'src/build', 'index.html'));
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
 app.listen(PORT, () => console.log(`Server Running in port ${PORT}`));
