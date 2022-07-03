423b8d19e99b92bf6cc893296c691f79a2b0018c
Rafaela Dartora <siglyane@gmail.com>
2022-07-02 21:22:56 -0300

    Favoritar e compartilhar

--- frontend/src/pages/conteudo/index.js
+++ frontend/src/pages/conteudo/index.js
@@ -1,26 +1,32 @@
+import YoutubeEmbed from "../../components/YoutubeEmbed";
+import { Video, Button } from "./styles";
+import { BsFillBookmarkFill, BsFillShareFill } from "react-icons/bs";
 
-import YoutubeEmbed from '../../components/YoutubeEmbed';
-import { Video} from './styles';
-function Home (){
-
-return(
+function Home() {
+  return (
     <div className="App">
-        <Video>
-            <YoutubeEmbed></YoutubeEmbed>
-        </Video>
-        <h3>Corrimento antes da menstruação: Quando se preocupar?</h3>
-        <h8>31 de janeiro de 2022</h8>
-        <h6>Lorem ipsum quis sollicitudin aenean ornare amet est suspendisse luctus lobortis,
-            ligula lorem elit habitant diam tristique vulputate praesent libero scelerisque,
-            morbi erat scelerisque sagittis integer conubia sollicitudin condimentum habitasse.</h6>
-            
-        
+      <Video>
+        <YoutubeEmbed></YoutubeEmbed>
+      </Video>
+      <h3>Corrimento antes da menstruação: Quando se preocupar?</h3>
+      <h8>31 de janeiro de 2022</h8>
+      <h6>
+        Lorem ipsum quis sollicitudin aenean ornare amet est suspendisse luctus
+        lobortis, ligula lorem elit habitant diam tristique vulputate praesent
+        libero scelerisque, morbi erat scelerisque sagittis integer conubia
+        sollicitudin condimentum habitasse.
+      </h6>
+      <div>
+        <Button>
+          <BsFillShareFill />
+          Compartilhar
+        </Button>
+        <Button>
+          <BsFillBookmarkFill /> Favoritar
+        </Button>
+      </div>
     </div>
-
-
-);
-
+  );
 }
 
-
-export default Home
+export default Home;
--- frontend/src/pages/conteudo/styles.js
+++ frontend/src/pages/conteudo/styles.js
@@ -1,15 +1,21 @@
 import styled from "styled-components";
 
-export const Video = styled.div `
-    {
-       
-        width: 100%;
-        z-index: 20;
-        height: 100%;
-        left: 0px;
-        top: 0px;
-        margin:auto
-          
-    }`
-    
+export const Video = styled.div`
+   {
+    width: 100%;
+    z-index: 20;
+    height: 100%;
+    left: 0px;
+    top: 0px;
+    margin: auto;
+  }
+`;
 
+export const Button = styled.button`
+  background-color: white;
+  border: none;
+  color: black;
+  background-image: linear-gradient(45deg, #ff00bb, #ea1b23);
+  -webkit-background-clip: text;
+  -webkit-text-fill-color: transparent;
+`;
