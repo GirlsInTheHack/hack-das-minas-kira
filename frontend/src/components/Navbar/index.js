05ce0ebf9d711565bda5329856b8475054e5f871
HannahFreitas <hannahcassia@gmail.com>
2022-07-02 21:52:20 -0300

    adicionando outra parte do menu

--- frontend/src/components/Navbar/index.js
+++ frontend/src/components/Navbar/index.js
@@ -2,9 +2,10 @@ import React, { useState } from 'react';
 import * as FaIcons from "react-icons/fa";
 import * as AiIcons from "react-icons/ai";
 import { NavbarData } from './navbarData';
-import { Nav, NavIcon, SideBarNav, SideBarWrap } from "./style";
+import { Hr, Nav, NavIcon, SideBarNav, SideBarWrap } from "./style";
 import { IconContext } from 'react-icons/lib';
 import SubMenu from './subMenu';
+import { NavbarDataTwo } from './navbarDataTwo';
 
 const Navbar = () => {
     const [navbar, setNavbar] = useState(false);
@@ -12,20 +13,24 @@ const Navbar = () => {
     const showNavbar = () => setNavbar(!navbar);
     return(
         <div>
-            <IconContext.Provider value={{color: '#626162'}}>
+            <IconContext.Provider value={{color: '#626162', size: '20px'}}>
             <Nav>
                 <NavIcon to="#">
                      <FaIcons.FaBars onClick={showNavbar}/>
                 </NavIcon>
             </Nav>
             </IconContext.Provider>
-            <IconContext.Provider value={{color: '#fff'}}>
+            <IconContext.Provider value={{color: '#fff', size: '20px'}}>
             <SideBarNav navbar={navbar}>
                 <SideBarWrap>
                         <NavIcon to="#" className="menu-bars">
                             <AiIcons.AiOutlineClose onClick={showNavbar}/>
                         </NavIcon>
                         {NavbarData.map((item, index) => {
+                            return <SubMenu item={item} key={index}/>
+                        })}
+                        <Hr/>
+                        {NavbarDataTwo.map((item, index) => {
                             return <SubMenu item={item} key={index}/>
                         })}
                 </SideBarWrap>
